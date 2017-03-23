4a427064c8bc8671aeee2ebd3cefd7e4e98498f1
Jackson Carter <jcarter0803@gmail.com>
2017-03-23 16:33:38 -0700

    Help please

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,78 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var stringified = [];
+    
+
+    if(typeof obj === 'string'){
+  		return "\"" + obj + "\"";
+  	} else if(typeof obj === 'number'){
+  		return "" + obj;
+  	} else if(obj === null || obj === 'undifend' || typeof obj === 'boolean'){
+  		return "" + obj;
+  	} 
+
+    else if (Array.isArray(obj)) {
+        if(obj[0] === undefined){
+            	return '[]';
+        }
+        else {
+            obj.forEach(function(element){
+            	stringified.push(stringifyJSON(element));
+            });
+          return '[' + stringified + ']';
+        }
+    }
+
+   
+ 	else if(typeof obj === 'object'){
+    
+    	
+     } 
+     else{
+
+     for (var el in obj) {
+        if (obj.hasOwnProperty(el)) {
+              stringifyJSON(el);
+         	}
+            if (el.toString() && obj[el].toString()) {
+                start += el.toString() + ": " + obj[el].toString();
+            }
+	         if (typeof obj[el] === 'object') {
+	            start += stringifyJSON(obj[el]);    
+	        }
+           
+  		}
+  	 }
+    }
+
+    return stringify + "}"
+
+
+    }
 };
+
+
+
+  // var stringify = "{";
+
+
+  //   
+  //       } 
+  //    else{
+  //    for (var el in obj) {
+  //       if (obj.hasOwnProperty(el)) {
+  //             stringifyJSON(el);
+  //        	}
+  //           if (el.toString() && obj[el].toString()) {
+  //               start += el.toString() + ": " + obj[el].toString();
+  //           }
+	 //         if (typeof obj[el] === 'object') {
+	 //            start += stringifyJSON(obj[el]);    
+	 //        }
+           
+  // 		}
+  // 	 }
+  //   }
+
+  //   return stringify + "}"
